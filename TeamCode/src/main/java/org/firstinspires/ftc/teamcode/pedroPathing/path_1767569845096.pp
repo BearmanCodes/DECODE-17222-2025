49b43d4be14ccb60db7833f8bb768bc7990ef660
Bearman <idostuff1225@gmail.com>
2026-01-04 18:38:09 -0500

    Create path_1767569845096.pp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/path_1767569845096.pp
@@ -0,0 +1,85 @@
+{
+  "startPoint": {
+    "x": 56,
+    "y": 8,
+    "heading": "linear",
+    "startDeg": 90,
+    "endDeg": 180,
+    "locked": false
+  },
+  "lines": [
+    {
+      "id": "line-n4cpi37qsv9",
+      "name": "Path 1",
+      "endPoint": {
+        "x": 56.515188335358445,
+        "y": 85.910085054678,
+        "heading": "linear",
+        "startDeg": 180,
+        "endDeg": 135,
+        "degrees": 179
+      },
+      "controlPoints": [],
+      "color": "#96A988",
+      "locked": false,
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mk0cvnog-5hg5k1",
+      "name": "Path 2",
+      "endPoint": {
+        "x": 17.5031847133758,
+        "y": 85.910085054678,
+        "heading": "constant",
+        "reverse": false,
+        "startDeg": 135,
+        "endDeg": 0,
+        "degrees": 0
+      },
+      "controlPoints": [],
+      "color": "#9D96D7",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    },
+    {
+      "id": "mk0cz4em-n6g8sk",
+      "name": "Path 3",
+      "endPoint": {
+        "x": 56.515188335358445,
+        "y": 85.910085054678,
+        "heading": "linear",
+        "reverse": false,
+        "startDeg": 0,
+        "endDeg": 135
+      },
+      "controlPoints": [],
+      "color": "#5B9C66",
+      "waitBeforeMs": 0,
+      "waitAfterMs": 0,
+      "waitBeforeName": "",
+      "waitAfterName": ""
+    }
+  ],
+  "shapes": [],
+  "sequence": [
+    {
+      "kind": "path",
+      "lineId": "line-n4cpi37qsv9"
+    },
+    {
+      "kind": "path",
+      "lineId": "mk0cvnog-5hg5k1"
+    },
+    {
+      "kind": "path",
+      "lineId": "mk0cz4em-n6g8sk"
+    }
+  ],
+  "version": "1.2.1",
+  "timestamp": "2026-01-04T23:37:30.331Z"
+}
